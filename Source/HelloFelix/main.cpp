c88fa26bfdd8ee1588e73fd025bf55ffbd215818
Robert Crosby <robmcrosby@gmail.com>
2016-03-22 15:23:36 -0700

    Added TextScene.xml to HelloFelix.

--- Source/HelloFelix/main.cpp
+++ Source/HelloFelix/main.cpp
@@ -24,7 +24,8 @@
 //#define SCENE_FILE "TriangleScene.xml"
 //#define SCENE_FILE "TextureScene.xml"
 //#define SCENE_FILE "FrameScene.xml"
-#define SCENE_FILE "DepthScene.xml"
+//#define SCENE_FILE "DepthScene.xml"
+#define SCENE_FILE "TextScene.xml"
 
 using namespace std;
 
