5f0b8032e73858c4506e357140dec750dcde2664
Robert Crosby <robmcrosby@gmail.com>
2017-08-04 16:25:23 -0700

    Created ARTracker project from XMLScene

---
+++ Projects/ARTracker/ARTracker/ARTracker.cpp
@@ -0,0 +1,36 @@
+//
+//  ARTracker.cpp
+//  ARTracker
+//
+//  Created by Robert Crosby on 8/4/17.
+//  Copyright © 2017 Robert Crosby. All rights reserved.
+//
+
+#include "ARTracker.h"
+#include <FelixEngine/Camera.h>
+#include <FelixEngine/Model.h>
+
+
+ARTracker::ARTracker() {
+  printf("Create ARTracker\n");
+}
+
+ARTracker::~ARTracker() {
+  
+}
+
+void ARTracker::initalize() {
+  _scene.loadXMLFile("Scene.xml");
+  _model = _scene.getModel("Model");
+}
+
+void ARTracker::update() {
+  _model->setOrientation(_model->orientation() * fx::quat::RotZ(0.02f));
+  _model->update();
+  
+  _scene.update();
+}
+
+void ARTracker::render() {
+  _scene.render();
+}
---
+++ Projects/ARTracker/ARTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARTracker
+//
+//  Created by Robert Crosby on 8/4/17.
+//  Copyright © 2017 Robert Crosby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
