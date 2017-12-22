b437b66b3e2e62f4efdf4cc8f6b4e600aa592a17
Robert Crosby <robmcrosby@gmail.com>
2017-12-21 17:05:21 -0800

    Added ModelViewer Project

---
+++ Projects/ModelViewer/ModelViewer/DetailView/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  ModelViewer
+//
+//  Created by Robert Crosby on 12/21/17.
+//  Copyright © 2017 Robert Crosby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
