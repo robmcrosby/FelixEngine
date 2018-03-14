9f521b86824d177e1048b16d904e8665f47f043f
Robert Crosby <robmcrosby@gmail.com>
2018-03-14 19:19:20 -0400

    Added MeshBuilder Files

---
+++ FelixEngine/FelixEngine/Systems/Graphics/MeshBuilder.cpp
@@ -0,0 +1,9 @@
+//
+//  MeshBuilder.cpp
+//  FelixEngine
+//
+//  Created by Robert Crosby on 3/14/18.
+//  Copyright © 2018 Robert Crosby. All rights reserved.
+//
+
+#include "MeshBuilder.h"
---
+++ FelixEngine/FelixEngine/Systems/Graphics/MeshBuilder.h
@@ -0,0 +1,20 @@
+//
+//  MeshBuilder.h
+//  FelixEngine
+//
+//  Created by Robert Crosby on 3/14/18.
+//  Copyright © 2018 Robert Crosby. All rights reserved.
+//
+
+#ifndef MeshBuilder_h
+#define MeshBuilder_h
+
+#include "GraphicResources.h"
+
+namespace fx {
+  struct MeshBuilder {
+    
+  };
+}
+
+#endif /* MeshBuilder_hpp */
