be62da8e05f4f47acc7869e7f539d7fd80761437
Robert Crosby <robmcrosby@gmail.com>
2022-09-18 11:38:12 -0700

    Added SPIR-V shader compilation
    Added copy shader
    Added Mac Converter program

--- Projects/Vulkan01_Compute/src/main.cpp
+++ Projects/Vulkan01_Compute/src/main.cpp
@@ -6,36 +6,36 @@ using namespace std;
 
 void runComputeTest(VulkanDevicePtr device, VulkanQueuePtr queue) {
   // Create an Array of random floats
-  array<float, 32> bufferSrc, bufferResult;
-  for (auto& f : bufferSrc)
+  array<float, 32> srcBuffer, resultBuffer;
+  for (auto& f : srcBuffer)
     f = (rand()/(RAND_MAX/10000))/100.0f;
 
   // Create A Vulkan Buffer for writing [CPU -> GPU]
-  auto bufferIn = device->createBuffer();
-  bufferIn->setUsage(VK_BUFFER_USAGE_TRANSFER_SRC_BIT);
-  bufferIn->setCreateFlags(VMA_ALLOCATION_CREATE_MAPPED_BIT |
+  auto inBuffer = device->createBuffer();
+  inBuffer->setUsage(VK_BUFFER_USAGE_TRANSFER_SRC_BIT);
+  inBuffer->setCreateFlags(VMA_ALLOCATION_CREATE_MAPPED_BIT |
     VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT );
 
   // Cerate A Vulkan Buffer to read from [GPU -> CPU]
-  auto bufferOut = device->createBuffer();
-  bufferOut->setUsage(VK_BUFFER_USAGE_TRANSFER_DST_BIT);
-  bufferOut->setCreateFlags(VMA_ALLOCATION_CREATE_MAPPED_BIT |
+  auto outBuffer = device->createBuffer();
+  outBuffer->setUsage(VK_BUFFER_USAGE_TRANSFER_DST_BIT);
+  outBuffer->setCreateFlags(VMA_ALLOCATION_CREATE_MAPPED_BIT |
     VMA_ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT);
 
   // Allocate the Vulkan Buffers
-  VkDeviceSize size = bufferSrc.size() * sizeof(bufferSrc[0]);
-  if (bufferIn->alloc(size) && bufferOut->alloc(size)) {
+  VkDeviceSize size = srcBuffer.size() * sizeof(srcBuffer[0]);
+  if (inBuffer->alloc(size) && outBuffer->alloc(size)) {
     // Wirte to the device
-    memcpy(bufferIn->data(), bufferSrc.data(), size);
+    memcpy(inBuffer->data(), srcBuffer.data(), size);
 
     auto command = queue->createCommand();
 
     cout << "It Works!" << endl;
 
     // Read from the device
-    memcpy(bufferResult.data(), bufferOut->data(), size);
-    for (int i = 0; i < bufferResult.size(); ++i)
-      cout << "  " << bufferSrc[i] << "\t-> " << bufferResult[i] << endl;
+    memcpy(resultBuffer.data(), outBuffer->data(), size);
+    for (int i = 0; i < resultBuffer.size(); ++i)
+      cout << "  " << srcBuffer[i] << "\t-> " << resultBuffer[i] << endl;
   }
   else {
     cerr << "Error: initalizing buffers" << endl;
