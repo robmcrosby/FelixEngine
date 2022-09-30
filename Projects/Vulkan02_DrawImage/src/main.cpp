a43fe35d5d2c38387f94f9b43ca8bd057e274a03
Robert Crosby <robmcrosby@gmail.com>
2022-09-29 18:21:11 -0700

    Added STB image library

--- Projects/Vulkan02_DrawImage/src/main.cpp
+++ Projects/Vulkan02_DrawImage/src/main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <vector>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
 
 using namespace std;
 
 
 int main() {
+  int width = 512;
+  int height = 512;
+  int channels = 4;
+  vector<uint8_t> img(width * height * channels, 0xff);
+
+  stbi_write_png("result.png", width, height, channels, img.data(), width * channels);
+  stbi_write_jpg("result.jpg", width, height, channels, img.data(), 100);
+
   cout << "It Works!" << endl;
   return 0;
 }
