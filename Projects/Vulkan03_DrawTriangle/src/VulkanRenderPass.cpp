02dc8d35152b80988f70f0450cdd27522976a998
Robert Crosby <robmcrosby@gmail.com>
2022-10-23 12:00:28 -0700

    Changes to subpass dependencies

--- Projects/Vulkan03_DrawTriangle/src/VulkanRenderPass.cpp
+++ Projects/Vulkan03_DrawTriangle/src/VulkanRenderPass.cpp
@@ -78,12 +78,13 @@ VkRenderPass VulkanRenderPass::createVkRenderPass() {
   subpass.pColorAttachments    = attachmentRefs.data();
 
   VkSubpassDependency dependency;
-  dependency.srcSubpass    = VK_SUBPASS_EXTERNAL;
-  dependency.dstSubpass    = 0;
-  dependency.srcStageMask  = 0; //VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
-  dependency.srcAccessMask = 0;
-  dependency.dstStageMask  = 0; //VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
-  dependency.dstAccessMask = 0; //VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+  dependency.srcSubpass      = VK_SUBPASS_EXTERNAL;
+  dependency.dstSubpass      = 0;
+  dependency.srcStageMask    = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+  dependency.srcAccessMask   = 0;
+  dependency.dstStageMask    = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+  dependency.dstAccessMask   = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+  dependency.dependencyFlags = 0;
 
   VkRenderPassCreateInfo renderPassInfo = {VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO};
   renderPassInfo.flags           = 0;
