0fef74f22cfb64b6864ecb828af0d4fd32ef3c45
Robert Crosby <robmcrosby@gmail.com>
2022-11-07 18:00:37 -0800

    Added SDL2 Framework for macOS
    Set to display a blank window in main method

--- Projects/Vulkan04_HelloSDL/src/main.cpp
+++ Projects/Vulkan04_HelloSDL/src/main.cpp
@@ -1,10 +1,41 @@
 #include "VulkanIncludes.hpp"
+#include "SDL.h"
 
 
 using namespace std;
 
 
-int main() {
-  cout << "It Works!" << endl;
+int main(int argc, char *argv[])
+{
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    cerr << "Error Initalizing SDL" << endl;
+    return 1;
+  }
+
+  SDL_Window *window = SDL_CreateWindow(
+    "Hello SDL",
+    SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+    640, 480,
+    0
+  );
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+  SDL_RenderClear(renderer);
+  SDL_RenderPresent(renderer);
+
+  bool quit = false;
+  SDL_Event e;
+  while (!quit) {
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT) {
+        quit = true;
+      }
+    }
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
   return 0;
 }
