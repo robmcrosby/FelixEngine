559469f94715c480e04e031fa8ab658ac4135d50
Robert Crosby <robmcrosby@gmail.com>
2022-12-12 21:41:01 -0800

    Started Tests with Vulkan

--- Projects/Vulkan06_VKProject/tests/graphics_test.cpp
+++ Projects/Vulkan06_VKProject/tests/graphics_test.cpp
@@ -1,20 +1,76 @@
 #include <gtest/gtest.h>
 #include <VulkanIncludes.hpp>
+#include <iostream>
 
 
-TEST(GraphicsTests, TestHelloWorld) {
-    const auto expected = 1;
-    const auto actual = 1;
-    ASSERT_EQ(expected, actual);
+using namespace std;
+
+class VulkanTest : public ::testing::Test {
+public:
+  VulkanDevicePtr mDevice;
+  VulkanQueuePtr mQueue;
+
+protected:
+   VulkanTest() {
+     //cout << "VulkanTest()" << endl;
+     auto& instance = VulkanInstance::Get();
+     instance.setApplicationName("Graphics Unit Tests");
+     instance.setEngineName("No Engine");
+     instance.enableValidation();
+     assert(instance.init());
+   }
+
+   ~VulkanTest() {
+     //cout << "~VulkanTest()" << endl;
+     VulkanInstance::Get().destroy();
+   }
+
+   void SetUp() override {
+     //cout << "SetUp()" << endl;
+     mDevice = VulkanInstance::Get().pickDevice();
+     assert(mDevice != nullptr);
+
+     mQueue = mDevice->createQueue(VK_QUEUE_COMPUTE_BIT);
+     assert(mQueue != nullptr);
+
+     assert(mDevice->init());
+  }
+
+  void TearDown() override {
+    //cout << "TearDown()" << endl;
+    mQueue = nullptr;
+    mDevice = nullptr;
+  }
+};
+
+
+TEST_F(VulkanTest, TestCreateBuffer) {
+  auto buffer = mDevice->createBuffer();
+  ASSERT_TRUE(buffer != nullptr);
+
+  buffer->setUsage(VK_BUFFER_USAGE_TRANSFER_SRC_BIT |
+    VK_BUFFER_USAGE_STORAGE_BUFFER_BIT);
+  buffer->setCreateFlags(VMA_ALLOCATION_CREATE_MAPPED_BIT |
+    VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT);
+  ASSERT_TRUE(buffer->alloc(64));
 }
 
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
+TEST_F(VulkanTest, TestCreateLayout) {
+  auto layout = mDevice->createLayout();
+  ASSERT_TRUE(layout != nullptr);
+}
 
-    auto& instance = VulkanInstance::Get();
-    instance.setApplicationName("Graphics Unit Tests");
-    instance.setEngineName("No Engine");
-    instance.enableValidation();
+TEST_F(VulkanTest, TestCreatePipeline) {
+  auto pipeline = mDevice->createPipeline();
+  ASSERT_TRUE(pipeline != nullptr);
+}
 
-    return RUN_ALL_TESTS();
+TEST_F(VulkanTest, TestCreateCommand) {
+  auto command = mQueue->createCommand();
+  ASSERT_TRUE(command != nullptr);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
