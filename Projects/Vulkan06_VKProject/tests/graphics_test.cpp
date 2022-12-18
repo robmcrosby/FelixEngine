a264a13051b185e0e18e32dd636889b14f7f88ba
Robert Crosby <robmcrosby@gmail.com>
2022-12-17 18:06:06 -0800

    Added a Compute Unit Test

--- Projects/Vulkan06_VKProject/tests/graphics_test.cpp
+++ Projects/Vulkan06_VKProject/tests/graphics_test.cpp
@@ -1,7 +1,10 @@
 #include <gtest/gtest.h>
 #include <VulkanIncludes.hpp>
 #include <iostream>
+#include <cstring>
+#include <vector>
 
+#define ARRAY_SIZE 64
 
 using namespace std;
 
@@ -23,6 +26,11 @@ protected:
    ~VulkanTest() {
      //cout << "~VulkanTest()" << endl;
      VulkanInstance::Get().destroy();
+   }
+
+   static void fillRandFloats(vector<float>& v) {
+     for (auto& f : v)
+       f = (rand()/(RAND_MAX/10000))/100.0f;
    }
 
    void SetUp() override {
@@ -68,6 +76,53 @@ TEST_F(VulkanTest, TestCreatePipeline) {
 TEST_F(VulkanTest, TestCreateCommand) {
   auto command = mQueue->createCommand();
   ASSERT_TRUE(command != nullptr);
+}
+
+TEST_F(VulkanTest, TestCompute) {
+  vector<float> src(ARRAY_SIZE, 0), result(ARRAY_SIZE, 0);
+  fillRandFloats(src);
+
+  // Buffer for writing to device [CPU -> GPU]
+  auto inBuffer = mDevice->createBuffer();
+  inBuffer->setUsage(VK_BUFFER_USAGE_TRANSFER_DST_BIT |
+    VK_BUFFER_USAGE_STORAGE_BUFFER_BIT);
+  inBuffer->setCreateFlags(VMA_ALLOCATION_CREATE_MAPPED_BIT |
+    VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT );
+
+  // Buffer for reading from device [GPU -> CPU]
+  auto outBuffer = mDevice->createBuffer();
+  outBuffer->setUsage(VK_BUFFER_USAGE_TRANSFER_SRC_BIT |
+    VK_BUFFER_USAGE_STORAGE_BUFFER_BIT);
+  outBuffer->setCreateFlags(VMA_ALLOCATION_CREATE_MAPPED_BIT |
+    VMA_ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT);
+
+  // Allocate the Vulkan Buffers and write to device
+  VkDeviceSize size = src.size() * sizeof(src[0]);
+  ASSERT_TRUE(inBuffer->alloc(size) && outBuffer->alloc(size));
+  memcpy(inBuffer->data(), src.data(), size);
+
+  auto layoutSet = mDevice->createLayoutSet();
+  auto layout = layoutSet->at(0);
+  layout->setBuffer(0, inBuffer);
+  layout->setBuffer(1, outBuffer);
+  layout->update();
+
+  auto pipeline = mDevice->createPipeline();
+  ASSERT_TRUE(pipeline->setKernalShader("shaders/copy.spv"));
+
+  auto command = mQueue->createCommand();
+  command->begin();
+  command->bind(pipeline, layoutSet);
+  command->dispatch(1);
+  command->end();
+
+  mQueue->submitCommand(command);
+  mQueue->waitIdle();
+
+  // Read from the device and check results
+  memcpy(result.data(), outBuffer->data(), size);
+  for (int i = 0; i < src.size(); ++i)
+    EXPECT_EQ(result[i], src[i]);
 }
 
 int main(int argc, char **argv) {
